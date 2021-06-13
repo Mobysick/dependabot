84d5a9e8bd6f9078ffcbf72ea49cb84fc94548b6
Mobysick <sd.emre.gonen@gmail.com>
2021-06-13 13:53:26 +0300

    Code refactor.

--- src/dependabot-core/dependency-parser/node-dependency-parser.service.ts
+++ src/dependabot-core/dependency-parser/node-dependency-parser.service.ts
@@ -4,15 +4,8 @@ import { Dependency } from './types/dependency.type';
 export class NodeDependencyParserService implements DependencyParserService {
   // TODO: Fix any.
   parse(data: any): Dependency[] {
-    const keys = Object.keys(data.dependencies).map((key) => ({
-      key,
-      version: data.dependencies[key],
-    }));
-    return keys.concat(
-      Object.keys(data.devDependencies).map((key) => ({
-        key,
-        version: data.devDependencies[key],
-      })),
-    );
+    return Object.entries(data.dependencies)
+      .concat(Object.entries(data.devDependencies))
+      .map(([key, version]) => ({ key, version: version as string }));
   }
 }
--- src/dependabot-core/dependency-parser/php-dependency-parser.service.ts
+++ src/dependabot-core/dependency-parser/php-dependency-parser.service.ts
@@ -4,12 +4,8 @@ import { Dependency } from './types/dependency.type';
 export class PhpDependencyParserService implements DependencyParserService {
   // TODO: Fix any.
   parse(data: any): Dependency[] {
-    const keys = Object.keys(data.require).map((key) => ({ key, version: data.require[key] }));
-    return keys.concat(
-      Object.keys(data['require-dev']).map((key) => ({
-        key,
-        version: data['require-dev'][key],
-      })),
-    );
+    return Object.entries(data.require)
+      .concat(Object.entries(data['require-dev']))
+      .map(([key, version]) => ({ key, version: version as string }));
   }
 }
